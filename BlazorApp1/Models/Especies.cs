7ab10abd1303f149b1ccea801010fcb6a6a2fbfd
ivano <ivano@DESKTOP-1EVG9M8>
2022-09-26 20:06:34 -0300

    Agregar archivos de proyecto.

---
+++ BlazorApp1/Models/Especies.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Especies
+    {
+        public string Animal { get; set; }
+        public string Color { get; set; }
+        public bool Volador { get; set; }
+
+        public Especies()
+        {
+            Animal = string.Empty;
+            Color = string.Empty;
+            Volador = false;
+        }
+    }
+}
---
+++ BlazorApp1/Servicios/IAnimales.cs
@@ -0,0 +1,36 @@
+﻿using BlazorApp1.Models;
+using System.Threading;
+
+namespace BlazorApp1.Servicios
+{
+    public interface IAnimales
+    {
+        public List<Especies> Listar();
+        public void AgregarAnimal(Especies especie);
+    }
+
+    public class Animales : IAnimales
+    {
+        private List<Especies> NuestrasEspecies=new List<Especies>();
+        public Animales()
+        {
+            Especies Tipo = new Especies();
+
+            Tipo.Animal = "Cochinillo";
+            Tipo.Volador = true;
+            Tipo.Color = "Rosadito";
+
+            NuestrasEspecies.Add(Tipo);
+        }
+
+        public List<Especies> Listar()
+        {
+            return NuestrasEspecies;
+        }
+
+        public void AgregarAnimal(Especies animal)
+        {
+            NuestrasEspecies.Add(animal);
+        }
+    }
+}
